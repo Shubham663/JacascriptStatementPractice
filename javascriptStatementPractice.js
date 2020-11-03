da098c452bf486d681785e9b8ba32e00492606aa
unknown <mittalshubham05@gmail.com>
2020-11-03 22:39:04 +0530

    Edit: add five two digit numbers and calculate average

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,2 +1,9 @@
-let digit = Math.floor(Math.random()*6)+1;
-console.log(digit);
+let avg = 0;
+let sum = 0;
+for(i=0;i<5;i++){
+    let val = Math.floor(Math.random()*100);
+    sum += val;
+}
+avg = sum/5;
+console.log(sum);
+console.log(avg);
