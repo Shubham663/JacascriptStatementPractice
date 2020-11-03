a9700a5367c5b0763572e4eac0b0dc82f10bdb4e
unknown <mittalshubham05@gmail.com>
2020-11-03 23:38:27 +0530

    Add: added functionality for checking whether date in specified range

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,9 +1,8 @@
-let max = 0;
-let min = 1000;
-for(i=0;i<5;i++){
-    let val = Math.floor(Math.random()*1000);
-    max=Math.max(max,val);
-    min=Math.min(min,val);
-}
-console.log("min val " + min);
-console.log("max val " + max);
+let day = 20;
+let month = 04;
+var d = new Date(2020,month-1,day);
+var d2 = new Date(2020,02,20);
+var d3 = new Date(2020,05,20);
+if(d > d2    &&    d < d3)
+    console.log("date present in range " + d2 + " to " + d3);
+// if()
