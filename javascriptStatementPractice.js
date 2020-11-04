9ee07cb481072d49cca5c04d05b2b433787cc166
unknown <mittalshubham05@gmail.com>
2020-11-04 16:01:17 +0530

    Edit: provided functionality for checking whether number and its pallindrome are prime

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,14 +1,33 @@
-let val = 14341;
+let val = 127;
 let temp =val;
 let sum=0
 
-while(temp >= 1){
-    let remainder = temp%10;
-    temp = Math.floor(temp/10);
-    sum = sum*10 + remainder;
+if(isPrime(val))
+    console.log("The number is prime");
+else
+    console.log("The number is not prime");
+
+let pallindrome = getPallindrome(val);
+
+if(isPrime(pallindrome))
+    console.log("The number's pallindrome is prime");
+else
+    console.log("The number's pallindrome is not prime");
+
+function getPallindrome(val){
+    let temp = val;
+    let sum = 0;
+    while(temp >= 1){
+        let remainder = temp%10;
+        temp = Math.floor(temp/10);
+        sum = sum*10 + remainder;
+    }
+    return sum;
 }
 
-if(sum == val)
-    console.log("The number is a pallindrome");
-else
-    console.log("The number is not a pallindrome");
+function isPrime(val){
+    for(let i=2; i <= Math.sqrt(val);i++)
+        if(val%i == 0)
+            return false;
+    return true;
+}
