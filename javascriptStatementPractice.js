04f708c7363d20d069be1dcd9df0310a88a49b76
unknown <mittalshubham05@gmail.com>
2020-11-04 14:52:36 +0530

    Edit: provided functionality for printing all prime in given range

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,12 +1,24 @@
-const n =27;
-let val = isPrime(n);
-if(val == true)
-    console.log("The number " + n + " is a prime number");
-else
-    console.log("The number " + n + " is not a prime number");
+const start =27;
+const end = 135;
+printPrimeNumbersInRange(start,end);
+
+
+// let val = isPrime(n);
+// if(val == true)
+//     console.log("The number " + n + " is a prime number");
+// else
+//     console.log("The number " + n + " is not a prime number");
+
+function printPrimeNumbersInRange(start,end){
+    for(let i=start ; i<=end ; i=i+1 )
+    {
+        if(isPrime(i))
+            console.log("The number " + i + " is prime.");
+    }
+}
 
 function isPrime(n){
-    for(i=2;i<=Math.floor(Math.sqrt(n));i++)
+    for(let i=2;i<=Math.floor(Math.sqrt(n));i++)
         if(n % i == 0)
             return false;
     return true;
