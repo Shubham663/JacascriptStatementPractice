82e02454d38828e25d496a4730dee252efe59821
unknown <mittalshubham05@gmail.com>
2020-11-04 13:12:00 +0530

    Edit: provided functionality for unit conversions

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,25 +1,7 @@
-let number = 10000;
-let digits = 0;
-while(number >= 1){
-    number/=10;
-    digits++;
-}
-switch(digits){
-    case 1:
-        console.log("One's digit");
-        break;
-    case 2:
-        console.log("Ten's digit");
-        break;
-    case 3:
-        console.log("Hundred's digit");
-        break;
-    case 4:
-        console.log("Thousand's digit");
-        break;
-    case 5:
-        console.log("Ten-Thousand's digit");
-        break;
-    default:
-        console.log("Value too large");
-}
+const feetToInch = 12;
+const feetToMeter = 0.3048;
+
+let inches = 36;
+console.log("Inches: " + inches);
+console.log("Feet: " + inches/feetToInch);
+console.log("Meters: " + (inches/feetToInch)*feetToMeter);
