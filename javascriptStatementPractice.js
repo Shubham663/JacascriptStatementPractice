ceca0e1e0693b6a0e17b892b22366356113d4264
unknown <mittalshubham05@gmail.com>
2020-11-04 15:52:51 +0530

    Edit: provided functionality for checking whether a number is a pallindrome

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,30 +1,14 @@
-let val = Math.floor(Math.random()*2);
-let temp = Math.floor(Math.random()*68)+32;
-const CELSIUS_LOWEST = 0;
-const CELSIUS_HIGHEST = 100;
-const FAHRENHEIT_LOWEST = 32;
-const FAHRENHEIT_HIGHEST = 212;
+let val = 14341;
+let temp =val;
+let sum=0
 
-let temp2 = convertTemperature(val,temp);
-if(temp2 != 300){
-    if(val == 0)
-        console.log("The temperature in fahrenheit is " + temp2);
-    else
-        console.log("The temperature in celsius is " + temp2);
+while(temp >= 1){
+    let remainder = temp%10;
+    temp = Math.floor(temp/10);
+    sum = sum*10 + remainder;
 }
-function convertTemperature(val,temp){
-    switch(val){
-    case 0: if(temp <CELSIUS_LOWEST || temp>CELSIUS_HIGHEST){
-                console.log("The temperature is not bw boiling and freezing point in degree Celsius");
-                return 300;
-            }
-            let f = temp*9/5 + 32;
-            return f;
-    case 1: if(temp<FAHRENHEIT_LOWEST || temp>FAHRENHEIT_HIGHEST){
-                console.log("The temperature is not bw boiling and freezing point in degree Fahrenheit");
-                return 300;
-            }  
-            let c = (temp-32)*5/9;
-            return c;
-    }
-}
+
+if(sum == val)
+    console.log("The number is a pallindrome");
+else
+    console.log("The number is not a pallindrome");
