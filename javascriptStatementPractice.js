ea4c866d5d6108d9212b816ad2fa1ff31b1be42c
unknown <mittalshubham05@gmail.com>
2020-11-04 13:06:38 +0530

    Edit: provided functionality for printing number's unit

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,19 +1,25 @@
-let number = Math.floor(Math.random()*7);
-switch(number){
-    case 0: console.log("SUNDAY");
-            break;
-    case 1: console.log("MONDAY");
-            break;
-    case 2: console.log("TUEDAY");
-            break;
-    case 3: console.log("WEDNESDAY");
-            break;
-    case 4: console.log("THURSDAY");
-            break;
-    case 5: console.log("FRIDAY");
-            break;
-    case 6: console.log("SATURDAY");
-            break;
-    default: console.log("value not correct");
-            break;
+let number = 10000;
+let digits = 0;
+while(number >= 1){
+    number/=10;
+    digits++;
+}
+switch(digits){
+    case 1:
+        console.log("One's digit");
+        break;
+    case 2:
+        console.log("Ten's digit");
+        break;
+    case 3:
+        console.log("Hundred's digit");
+        break;
+    case 4:
+        console.log("Thousand's digit");
+        break;
+    case 5:
+        console.log("Ten-Thousand's digit");
+        break;
+    default:
+        console.log("Value too large");
 }
