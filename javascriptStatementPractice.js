b8aed20b0408f16132e6a71ece42f96f5b452f1a
unknown <mittalshubham05@gmail.com>
2020-11-04 12:41:20 +0530

    Edit: provided funcionality for getting maximum of values

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,18 +1,12 @@
-let number = 1;
-let digits = 0;
-while(number >= 1){
-    number/=10;
-    digits++;
-}
-if(digits == 1)
-    console.log("One's digit");
-else if(digits == 2)
-    console.log("Ten's digit");
-else if(digits == 3)
-    console.log("Hundred's digit");
-else if(digits == 4)
-    console.log("Thousand's digit");
-else if(digits == 5)
-    console.log("Ten-Thousand's digit");
-else
-    console.log("Value too large");
+let number1 = 4;
+let number2 = 6;
+let number3 = 9;
+let val1 = number1 + number2*number3;
+let val2 = number1%number2 + number3;
+let val3 = number3 + number1/number2;
+let val4 = number3 + number1*number2;
+
+let max1= Math.max(val1,val2);
+let max2= Math.max(val3,val4);
+let max3 = Math.max(max1,max2);
+console.log(max3);
