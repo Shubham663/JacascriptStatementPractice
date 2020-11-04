826b2a5b62431fb51b7e33960b99b7589c16317f
unknown <mittalshubham05@gmail.com>
2020-11-04 11:48:38 +0530

    Edit provided functionality for printing digit in word

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,10 +1,21 @@
-let flip = flipCoin();
-console.log(flip);
-
-function flipCoin(){
-    let flip = Math.floor(Math.random()*2)+1;
-    if(flip == 1)
-        return "Heads";
-    else if(flip == 2)
-        return "Tails";
-}
+let number = Math.floor(Math.random()*10);
+if(number == 0)
+    console.log("zero");
+else if(number == 1)
+    console.log("one");
+else if(number == 2)
+    console.log("two");
+else if(number == 3)
+    console.log("three");
+else if(number == 4)
+    console.log("four");
+else if(number == 5)
+    console.log("five");
+else if(number == 6)
+    console.log("six");
+else if(number == 7)
+    console.log("seven");
+else if(number == 8)
+    console.log("eight");
+else if(number == 9)
+    console.log("nine");
