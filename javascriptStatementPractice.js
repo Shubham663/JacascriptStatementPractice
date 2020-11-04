c61fc4349a3808f028acf6ad37dc80b7a03ba266
unknown <mittalshubham05@gmail.com>
2020-11-04 14:55:45 +0530

    Edit provided functionality for getting factorial

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,25 +1,9 @@
-const start =27;
-const end = 135;
-printPrimeNumbersInRange(start,end);
+const val = 5;
+fac = getFactorial(val);
+console.log(fac);
 
-
-// let val = isPrime(n);
-// if(val == true)
-//     console.log("The number " + n + " is a prime number");
-// else
-//     console.log("The number " + n + " is not a prime number");
-
-function printPrimeNumbersInRange(start,end){
-    for(let i=start ; i<=end ; i=i+1 )
-    {
-        if(isPrime(i))
-            console.log("The number " + i + " is prime.");
-    }
-}
-
-function isPrime(n){
-    for(let i=2;i<=Math.floor(Math.sqrt(n));i++)
-        if(n % i == 0)
-            return false;
-    return true;
+function getFactorial(val){
+    if(val ==2 || val ==1)
+        return val;
+    return val*getFactorial(val-1);
 }
