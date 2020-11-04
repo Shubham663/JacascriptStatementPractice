4f3ec1b766644c869108270ff2fdf972bf92eb97
unknown <mittalshubham05@gmail.com>
2020-11-04 11:36:43 +0530

    Edit: provided functionality for getting coin flip result

--- javascriptStatementPractice.js
+++ javascriptStatementPractice.js
@@ -1,19 +1,10 @@
-let year = 2100;
-if(isLeapYear(year))
-    console.log("The year " + year + " is a leap year");
-else
-    console.log("The year " + year + " is not a leap year");
+let flip = flipCoin();
+console.log(flip);
 
-function isLeapYear(year){
-    let digits = 0;
-    let temp = year;
-    while(temp >= 1){
-        digits++;
-        temp/=10;
-    }
-    if(digits == 4 && year%4 == 0){
-        if(year%100 !=0 || (year%100 == 0 && year%400 == 0))
-            return true;
-    }
-    return false;
+function flipCoin(){
+    let flip = Math.floor(Math.random()*2)+1;
+    if(flip == 1)
+        return "Heads";
+    else if(flip == 2)
+        return "Tails";
 }
