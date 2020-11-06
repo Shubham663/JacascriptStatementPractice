def18d596265011d60032347e7e9ebdbecad8b77
unknown <mittalshubham05@gmail.com>
2020-11-06 17:28:35 +0530

    Add: provided functionality for using dictionary to map value to number of times it shows on dice

---
+++ dieRoll.js
@@ -0,0 +1,40 @@
+var dictionary = {};
+let max = 0;
+let min = 11;
+function occurence(key,value){
+    console.log(key + " " + value);
+    return false;
+}
+
+while(true){
+    let die = Math.floor(Math.random()*6) + 1;
+    if(dictionary[die] == undefined)
+        dictionary[die] = 1;
+    else{
+        let val = dictionary[die];
+        val++;
+        dictionary[die] = val;
+        if(val == 10)
+            break;
+    }
+}
+for(let i=1; i <= 6 ; i++){
+    if(dictionary[i] < min)
+        min = dictionary[i];
+    if(dictionary[i] > max)
+        max = dictionary[i];
+}
+let min1=0;
+let max1 = 0;
+for(let i=0;i<=6;i++){
+    if(dictionary[i] == min)
+        min1 = i;
+    if(dictionary[i] == max)
+        max1 = i;
+}
+    
+    for(let i=0;i<=6;i++){
+        occurence(i,dictionary[i]);
+    }
+    console.log("The value that came maximum=10 times: " + max1);
+    console.log("The value that came minimum=" + min + " times: " + min1);
