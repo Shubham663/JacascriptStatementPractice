fb99fb306d1c63a195d233ca6e6e39c8724ba71b
unknown <mittalshubham05@gmail.com>
2020-11-06 12:53:45 +0530

    Add: added functionality for getting second smallest and second largest elements

--- 2ndLargestAndSmallest.js
+++ 2ndLargestAndSmallest.js
@@ -4,3 +4,28 @@ for(let i=0;i<10;i++){
     numberArray.push(number);
 }
 console.log(numberArray);
+let smallest = 1000;
+let secondSmallest = 1000;
+let largest = 0;
+let secondlargest = 0;
+for(let i=0;i<numberArray.length;i++){
+    let val = numberArray[i];
+    if( val < secondSmallest){
+        if(val < smallest){
+            secondSmallest = smallest;
+            smallest = val;
+        }
+        else
+        secondSmallest = val;
+    }
+    if( val > secondlargest){
+        if(val > largest){
+            secondlargest = largest;
+            largest = val;
+        }
+        else
+        secondlargest = val;
+    }
+}
+console.log("Second smallest element " + secondSmallest);
+console.log("Second largest element: " + secondlargest);
